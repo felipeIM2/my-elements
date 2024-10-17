0c12999e9b5932f6806f23a8b26a4aab97d96f67
Luis Felipe Moreira <112032955+felipeIM2@users.noreply.github.com>
2024-10-17 18:04:47 -0300

    update

---
+++ script.js
@@ -0,0 +1,102 @@
+const contextMenu = document.getElementById("contextMenu");
+
+
+
+// Mostrar o menu de contexto ao clicar com o botão direito
+document.addEventListener("contextmenu", function (e) {
+   
+  const elementoClicado = e.target 
+  
+  const verifyModal = document.getElementById('modal')
+  const 
+
+  if(verifyModal.className === 'modalOff' && elementoClicado.className === 'line' ){
+    e.preventDefault(); 
+    contextMenu.style.display = "block";
+    contextMenu.style.left = e.pageX + "px";
+    contextMenu.style.top = e.pageY + "px";
+
+  }else if(verifyModal.className != 'modalOff'){
+      let modal = document.getElementById("modal")
+     // modal.classList = "modalOn"
+    alert("modal esta on")
+  }
+
+  
+  //console.log(elementoClicado)
+
+
+
+
+// Adicionar eventos de clique nas opções do menu
+contextMenu.querySelectorAll("li").forEach(function (item) {
+    item.addEventListener("click", function () {
+
+      
+      let items = item.innerText.toLocaleLowerCase();
+      let text = elementoClicado.innerText 
+
+      if(items === "editar"){
+        
+        let modal = document.getElementById("modal")
+        modal.classList = "modalOn"
+
+        let nameModal = document.getElementById("nameModal")
+        nameModal.innerText = items.toLocaleUpperCase();
+
+        let input = document.getElementById('altertext')
+        input.value = text
+
+        let sendButton = document.getElementById('catch')
+        sendButton.addEventListener('click', () => {
+          console.log("okenvia")
+        });
+
+      
+      } else if(items === "alterar"){
+        
+        let modal = document.getElementById("modal")
+        modal.classList = "modalOn"
+
+        let input = document.getElementById('altertext')
+        input.value = text
+
+        let nameModal = document.getElementById("nameModal")
+        nameModal.innerText = items.toLocaleUpperCase();
+
+
+      }
+
+
+      let cancelar = document.getElementById('back');
+      cancelar.addEventListener("click", () => verifyModal.classList ='modalOff');
+
+      contextMenu.style.display = "none";
+    });
+    
+  });
+
+});
+
+
+
+document.addEventListener("click", function () {
+  contextMenu.style.display = "none";
+
+   const verifyModal = document.getElementById('modal');
+   const popup = document.querySelector('.popup');
+
+    verifyModal.addEventListener("click", ()=>{
+      if(verifyModal.className === 'modalOn'){
+        verifyModal.classList = "modalOff"
+      }
+    })
+    popup.addEventListener("click", (event) => {
+      event.stopPropagation();
+    });
+
+});
+
+
+
+
